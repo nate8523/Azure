bf6400f3148d7b966ea43a05d59f83de38388e35
Nathan Carroll <33833899+nate8523@users.noreply.github.com>
2022-05-04 22:58:34 +0100

    Migrated Directory

--- Quick-Deploy/ADDS/Customisation-Scripts/InstallADDS.ps1
+++
@@ -1,8 +0,0 @@
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementtools
-
-#Create NEW AD Forest and Domain
-param ($DomainFQDN)
-param ($DSRMPassword)
-$DomainNetBios    =  $DomainFQDN.Split('.') | Select-Object -First 1
-$SafeModePassword = ConvertTo-SecureString $DSRMPassword -AsPlainText -Force
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath C:\Windows\NTDS -DomainMode WinThreshold -DomainName $DomainFQDN -DomainNetbiosName $DomainNetBios -ForestMode WinThreshold -InstallDns:$true -LogPath C:\Windows\NTDS -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $SafeModePassword -SysvolPath C:\Windows\SYSVOL -Force:$true
